f6b104bd33baade324fb2b67bbed0958ea2d701d
Asad Ali <asadrajput2@gmail.com>
2020-11-04 19:06:25 +0530

    added yasg for api documentation

--- dalcyn/urls.py
+++ dalcyn/urls.py
@@ -14,10 +14,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Dalcyn Backend API",
+        default_version='v0.1',
+        description="API documentation for dalcyn backend. Root urls is '/api'",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="asadrajput2@gmail.com"),
+        license=openapi.License(name="None"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
+api_doc_urls = [
+    re_path(r'^api_doc_swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^api_doc_swagger/$', schema_view.with_ui('swagger',
+                                                       cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^api_doc/$', schema_view.with_ui('redoc',
+                                               cache_timeout=0), name='schema-redoc'),
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authentication.urls')),
-    path('', include('backend.urls')),
-]
+    path('api/', include('backend.urls')),
+] + api_doc_urls
