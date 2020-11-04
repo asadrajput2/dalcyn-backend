31b40307e814cfcb9a15c5ed405d54134d7b5bec
Asad Ali <asadrajput2@gmail.com>
2020-11-04 19:30:53 +0530

    added URLs for APIs

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,21 @@
-from django.urls import path
-from .views import index
+from django.urls import path, include
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'crop', CropViewSet)
+router.register(r'farm', FarmViewSet, basename='farm')
+router.register(r'top_level',
+                TopLevelViewSet, basename='top_level')
+router.register(r'middle_level',
+                MiddleLevelViewSet, basename='middle_level')
+router.register(r'bottom_level',
+                BottomLevelViewSet, basename='bottom_level')
+router.register(r'prediction',
+                PredictionViewSet, basename='prediction')
 
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('', include(router.urls)),
 ]
